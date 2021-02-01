8186de1cce9029b09cfad197c2b9e797b274a036
jba-tm <jumageldiyewb@list.ru>
2021-02-01 19:19:52 +0500

    Chapter 9

---
+++ components/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+      <h1 className="bg-primary text-white text-center p-2">
+          Hello Adam
+      </h1>
+  );
+}
+
+export default App;
--- projecttools/src/App.js
+++ projecttools/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Display from "./Display";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: 'London'
+        }
+    }
+
+    changeCity = () => {
+        debugger
+        this.setState({ city: this.state.city === "London" ? "New York" : "London"})
+    }
+
+    render() {
+        return (
+            <Display value={ this.state.city } callback={ this.changeCity } />
+        );
+    }
 }
 
 export default App;
