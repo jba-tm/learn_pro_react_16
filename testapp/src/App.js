b3f512b8953e165f66ad12f25e211994ee7201d6
jba-tm <jumageldiyewb@list.ru>
2021-02-08 17:37:43 +0500

    Chapter 17

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Result} from "./Result";
+import {ValueInput} from "./ValueInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.title || "Simple Addition",
+            fieldValues: [],
+            total: 0,
+        }
+    }
+
+    updateFieldValue = (id, value) => {
+        let fieldValues = this.state.fieldValues
+        fieldValues[id]=Number(value)
+        this.setState({
+            fieldValues: fieldValues
+        })
+    }
+
+    updateTotal = () => {
+        this.setState({
+            total: this.state.fieldValues.reduce((total, val) => total += val, 0)
+        })
+    }
+
+    render() {
+        // throw new Error("something went wrong");
+        return (
+            <div className="m-2">
+                <h5 className="bg-primary text-white text-center p-2">
+                    {this.state.title}
+                </h5>
+                <Result result={this.state.total}/>
+                <ValueInput id="1" changeCallback={this.updateFieldValue}/>
+                <ValueInput id="2" changeCallback={this.updateFieldValue}/>
+                <ValueInput id="3" changeCallback={this.updateFieldValue}/>
+                <div className="text-center">
+                    <button onClick={this.updateTotal} className="btn btn-primary">
+                        Total
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;
